b6a5df75d60cc8689693095f1dae1b50f93f3735
Emmanuel Lampe <mail@emmanuel-lampe.de>
2021-06-23 22:42:57 +0200

    added scoreboard team support added chat packet modifier support

--- api/src/main/java/eu/miopowered/nickapi/listener/PacketListener.java
+++ api/src/main/java/eu/miopowered/nickapi/listener/PacketListener.java
@@ -2,6 +2,7 @@ package eu.miopowered.nickapi.listener;
 
 import com.mojang.authlib.GameProfile;
 import eu.miopowered.nickapi.NickImplementation;
+import eu.miopowered.nickapi.identity.Identity;
 import eu.miopowered.nickapi.user.NickUser;
 import eu.miopowered.nickapi.utility.tinyprotocol.Reflection;
 import eu.miopowered.nickapi.utility.tinyprotocol.TinyProtocol;
@@ -9,6 +10,7 @@ import io.netty.channel.Channel;
 import lombok.experimental.Accessors;
 import org.bukkit.entity.Player;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -30,6 +32,20 @@ public class PacketListener extends TinyProtocol {
     private static final Reflection.FieldAccessor<?> PLAYER_LIST_ITEM_ACTION_FIELD = Reflection.getField(PLAYER_LIST_ITEM_PACKET_CLASS, "a", PLAYER_INFO_ACTION_ENUM);
     private static final Reflection.MethodInvoker PLAYER_INFO_ACTION_NAME_METHOD = Reflection.getMethod(PLAYER_INFO_ACTION_ENUM, "name");
 
+    // Scoreboard team packet
+    private static final Class<?> SCOREBOARD_TEAM_PACKET = Reflection.getClass("{nms}.PacketPlayOutScoreboardTeam");
+    private static final Reflection.FieldAccessor<Collection> SCOREBOARD_TEAM_PLAYERS_FIELD = Reflection.getField(SCOREBOARD_TEAM_PACKET, "g", Collection.class);
+
+    // Chat packet
+    private static final Class<?> CHAT_PACKET = Reflection.getClass("{nms}.PacketPlayOutChat");
+    private static final Class<?> I_CHAT_BASE_COMPONENT_CLASS = Reflection.getClass("{nms}.IChatBaseComponent");
+    private static final Reflection.MethodInvoker I_CHAT_BASE_COMPONENT_SIBLINGS_METHOD = Reflection.getMethod(I_CHAT_BASE_COMPONENT_CLASS, "a");
+
+    private static final Reflection.FieldAccessor<?> CHAT_PACKET_COMPONENT_FIELD = Reflection.getField(CHAT_PACKET, "a", I_CHAT_BASE_COMPONENT_CLASS);
+    private static final Class<?> TEXT_COMPONENT_CLASS = Reflection.getClass("{nms}.ChatComponentText");
+    private static final Reflection.FieldAccessor<String> TEXT_COMPONENT_TEXT_FIELD = Reflection.getFinalField(TEXT_COMPONENT_CLASS, "b", String.class);
+
+
     private NickImplementation implementation;
 
     public PacketListener(NickImplementation implementation) {
@@ -45,7 +61,53 @@ public class PacketListener extends TinyProtocol {
         if (PLAYER_LIST_ITEM_PACKET_CLASS.isInstance(packet)) {
             this.handlePlayerListItem(receiver, packet);
         }
+        if (SCOREBOARD_TEAM_PACKET.isInstance(packet)) {
+            this.handleScoreboardTeams(receiver, packet);
+        }
+        if (CHAT_PACKET.isInstance(packet)) {
+            this.handleChat(receiver, packet);
+        }
         return packet;
+    }
+
+    private void handleChat(Player receiver, Object packet) {
+        Object o = CHAT_PACKET_COMPONENT_FIELD.get(packet);
+        if (I_CHAT_BASE_COMPONENT_CLASS.isInstance(o)) {
+            modify(receiver, o);
+        }
+    }
+
+    private void modify(Player player, Object base) {
+        if (TEXT_COMPONENT_CLASS.isInstance(base)) {
+            final String[] message = { TEXT_COMPONENT_TEXT_FIELD.get(base) };
+            this.implementation.users()
+                    .stream()
+                    .filter(nickUser -> message[0].contains(nickUser.realIdentity().username()))
+                    .filter(nickUser -> nickUser.filters().stream().noneMatch(filter -> filter.filter(player)))
+                    .forEach(nickUser -> message[0] = (message[0].replace(nickUser.realIdentity().username(), nickUser.fakeIdentity().username())));
+
+            TEXT_COMPONENT_TEXT_FIELD.set(base, message[0]);
+        }
+        Collection<Object> objects = (Collection<Object>) I_CHAT_BASE_COMPONENT_SIBLINGS_METHOD.invoke(base);
+
+        objects.forEach(o -> modify(player, o));
+    }
+
+    private void handleScoreboardTeams(Player receiver, Object packet) {
+        Collection<String> collection = (Collection<String>) SCOREBOARD_TEAM_PLAYERS_FIELD.get(packet);
+        collection.stream().forEach(s -> this.implementation
+                .users()
+                .stream()
+                .filter(nickUser -> nickUser.realIdentity().username().equalsIgnoreCase(s))
+                .filter(nickUser -> nickUser.filters().stream().noneMatch(filter -> filter.filter(receiver)))
+                .map(NickUser::fakeIdentity)
+                .map(Identity::username)
+                .findAny()
+                .ifPresent(nickUser -> {
+                    collection.remove(s);
+                    collection.add(nickUser);
+                }));
+        SCOREBOARD_TEAM_PLAYERS_FIELD.set(packet, collection);
     }
 
     private void handlePlayerListItem(Player receiver, Object packet) {
